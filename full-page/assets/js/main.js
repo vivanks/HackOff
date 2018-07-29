1a7a0861a9894747bf6a8d1c573294452e46102c
vivanks <vivanksharma@ymail.com>
2018-07-29 12:53:26 +0530

    navigation works

--- full-page/assets/js/main.js
+++ full-page/assets/js/main.js
@@ -26,6 +26,21 @@ jQuery(document).ready(function ($) {
         }
     });
 
+     $('.navbar-collapse').find('a[href*=#]:not([href=#])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: (target.offset().top - 40)
+            }, 1000);
+            if ($('.navbar-toggle').css('display') != 'none') {
+                $(this).parents('.container').find(".navbar-toggle").trigger("click");
+            }
+            return false;
+        }
+    }
+});
 
 
     /*---------------------------------------------*
